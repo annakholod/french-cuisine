13b169a10d46101ca34dd1cce937c53021bf1ac0
Anna Kholod <uashop89@gmail.com>
2020-03-01 10:06:35 +0200

    Updates

---
+++ precache-manifest.561abd1f048ce18aa456eef7bfcba328.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61972282addbb576e919f465b6c3bfc",
+    "url": "/french-cuisine/index.html"
+  },
+  {
+    "revision": "2a87d3f3dbaa20a4f554",
+    "url": "/french-cuisine/static/css/main.efd85457.chunk.css"
+  },
+  {
+    "revision": "021a398308732be0d505",
+    "url": "/french-cuisine/static/js/2.bf07e891.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/french-cuisine/static/js/2.bf07e891.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a87d3f3dbaa20a4f554",
+    "url": "/french-cuisine/static/js/main.5808ab84.chunk.js"
+  },
+  {
+    "revision": "4e229616baa9f8b89021",
+    "url": "/french-cuisine/static/js/runtime-main.d6c371ac.js"
+  },
+  {
+    "revision": "bb6a408913166f37105e47fbdb15548e",
+    "url": "/french-cuisine/static/media/about-1.bb6a4089.jpg"
+  },
+  {
+    "revision": "dfc1be809ee08de923fe55f7b668d95a",
+    "url": "/french-cuisine/static/media/about-2.dfc1be80.jpg"
+  },
+  {
+    "revision": "0631ba3ccb432957f7888c4bdb6b29d9",
+    "url": "/french-cuisine/static/media/about-3.0631ba3c.jpg"
+  },
+  {
+    "revision": "108d5c02c4b7a399542fe92a1ac30515",
+    "url": "/french-cuisine/static/media/about-4.108d5c02.jpg"
+  },
+  {
+    "revision": "98b8e6bef3954563e32044fd24e6092f",
+    "url": "/french-cuisine/static/media/header.98b8e6be.jpg"
+  },
+  {
+    "revision": "79a78b7805d1055b27880baa6c9e00dc",
+    "url": "/french-cuisine/static/media/menu-1.79a78b78.jpg"
+  },
+  {
+    "revision": "8de200045f3d657180e5a3d62697d105",
+    "url": "/french-cuisine/static/media/menu-2.8de20004.jpg"
+  },
+  {
+    "revision": "81b5cd695c2b0cf1f15507ea1782ae5e",
+    "url": "/french-cuisine/static/media/menu-3.81b5cd69.jpg"
+  }
+]);
